f1e5180ed54436bb0730afd6c58213f36e198003
Syed Razvi <razvisyed2002@gmail.com>
2026-08-22 16:21:19 +0530

    setting up alembic

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "postgresql://billing_user:billing_pass@localhost:5432/billing_engine"
+DATABASE_URL = "postgresql://postgres:rsadnan020626@localhost:5432/billing_engine"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit = False, autoflush=False, bind=engine)
